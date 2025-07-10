3850b9951d84444fe1d8275396467795da24ea94
Your Name <shahedali2001>
2025-07-10 23:08:08 +0530

    rg-added

--- main.tf
+++ main.tf
@@ -2,5 +2,10 @@ resource "azurerm_resource_group" "rgblcok" {
  name = "my-rg"
  location = "central india"
  
-  
+}
+
+resource "azurerm_resource_group" "rgblcok" {
+ name = "my-rg-003"
+ location = "central india"
+ 
 }
